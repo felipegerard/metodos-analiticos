48c863537422306507258eda46e3bb2407e943f1
FelipeGerard <felipegerard@gmail.com>
2015-05-04 15:10:01 -0500

    Un repositorio por persona

---
+++ felipe/process/txt_2_json.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+tr -d '\r' \
+| tr '\n' '|' \
+| sed -e 's/||/ <br> <br> /g' \
+      -e 's/|/ <br> /g' \
+| sed -E -e 's/ <br> <br> ([A-Z]+;? )/||\1/g' \
+| tr '|' '\n' \
+| grep -E "^[A-Z]+;?" \
+| sed -e 's/\./<punto>/g' \
+      -e 's/,/<coma>/g' \
+      -e 's/:/<dos_puntos>/g' \
+      -e 's/;/<punto_coma>/g' \
+      -e 's/\*/<asterisco>/g' \
+      -e 's/"/<comillas_dobles>/g' \
+      -e "s/'/<comillas_simples>/g" \
+      -e 's/`/<backtick>/g' \
+      -e 's/#/<gato>/g' \
+      -e 's/\[/<abre_corchetes>/g' \
+      -e 's/\]/<cierra_corchetes>/g' \
+      -e 's/(/<abre_parent>/g' \
+      -e 's/)/<cierra_parent>/g' \
+      -e 's/-/<guion>/g' \
+      -e 's/&/<ampersand>/g' \
+      -e 's/\//<diagonal>/g' \
+      -e 's/[^0-9a-zA-Z_<> ]//g' \
+| sed -E -e 's/^([A-Z]+)(<punto_coma> [A-Z]+)? <br> /"\1\2":"/' \
+	 -e 's/$| $/"/' \
+| grep '^"' \
+| sed -e '$ ! s/$/,/' \
+| awk 'BEGIN {print "{"} {print} END {print "}"}'
