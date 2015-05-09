6e223d8e121f97be67831590150bf43295a5cc3d
Omar Díaz Landa <omardiazlanda@gmail.com>
2015-05-09 15:00:14 -0500

    aplicacion Shiny con todo y tabsets, faltan algunos detatlles

--- omar/App_Shiny/ui.R
+++ omar/App_Shiny/ui.R
@@ -1,24 +1,57 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for dataset viewer application
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Hello World!"),
+  titlePanel("Words Recommendation"),
   
-  # Sidebar with a slider input for the number of bins
+  # Sidebar with controls to provide a caption, select a dataset,
+  # and specify the number of observations to view. Note that
+  # changes made to the caption in the textInput control are
+  # updated in the output area immediately as you type
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 5,
-                  max = 50,
-                  value = 30)
+      textInput("word", "Palabra o Frase:", "Analytic Methods"),
+      
+      submitButton("Update View")
     ),
     
-    # Show a plot of the generated distribution
+    
+    # Show the caption, a summary of the dataset and an HTML 
+    # table with the requested number of observations
     mainPanel(
-      plotOutput("distPlot")
+      h3(textOutput("word", container = span)),
+      tabsetPanel(type="tabs",
+                  tabPanel("Recommendation",tableOutput("res")),
+                  tabPanel("Discrimination", plotOutput("distPlot")),
+                  tabPanel("Words Contribution",plotOutput("contPlot"))
+                  
+                  
+                  
+                  
+                  )
+      #h3(textOutput("word", container = span)),
+      #tableOutput("res"),
+      #plotOutput("distPlot"),
+      #plotOutput("contPlot")
     )
   )
 ))
+
+
+
+
+#mainPanel(
+#  tabsetPanel(type = "tabs", 
+#              tabPanel("Plot", plotOutput("plot")), 
+#              tabPanel("Summary", verbatimTextOutput("summary")), 
+#              tabPanel("Table", tableOutput("table"))
+#)
+#  )
+#)
+
+
+
+
+
